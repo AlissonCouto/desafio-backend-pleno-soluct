4c0579275ec9e758c46bf2504ffb950dc313f413
Alisson Couto <allison.couto@phooto.com.br>
2025-06-27 15:44:45 -0400

    feat (api): Adicionando versionamento v1 nas rotas da API

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TaskController;
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout']);
+Route::prefix('v1')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('tasks', TaskController::class);
-    Route::get('tasks/{id}/history', [TaskController::class, 'history']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::apiResource('tasks', TaskController::class);
+        Route::get('tasks/{id}/history', [TaskController::class, 'history']);
+    });
 });
